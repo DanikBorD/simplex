6beb522de40fbac165df4e3fa90af4d778eefbd2
Daniil Borisenko <danikbord@ya.ru>
2018-05-22 22:29:52 +0500

    22.05.18

---
+++ simplex_method/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace simplex_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text_table = string.Empty;
+            string path = @"D:\simplex.txt"; // !!! путь до файла с исходниками
+            var rows = new List<string>();
+            var columns = new List<string>();
+
+            if (System.IO.File.Exists(path))
+                text_table = System.IO.File.ReadAllText(path);
+
+            rows = text_table.Split(';').ToList();
+
+            if (rows.Last() == "")
+                rows.RemoveAt(rows.IndexOf(rows.Last()));
+
+            columns = rows[0].Split(',').ToList();
+            double[,] table = new double[rows.Count, columns.Count];
+
+            for (var str = 0; str < rows.ToArray().Length; str++)
+                for (var col = 0; col < columns.ToArray().Length; col++)
+                    table[str, col] = Convert.ToDouble(rows[str].Split(',')[col]);
+
+            //double[,] table = { {25, -3,  5}, (25 >= -3x1 + 5x2)
+            //                    {30, -2,  5}, 
+            //                    {10,  1,  0}, 
+            //                    { 6,  3, -8}, 
+            //                    { 0, -6, -5} }; // целевое уравнение вконце
+
+            Console.WriteLine("Исходная система:");
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                for (int j = 0; j < table.GetLength(1); j++)
+                    Console.Write(table[i, j] + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //double[] result = new double[10];
+            int result_size = 0;
+            double[,] table_result;
+            Simplex S = new Simplex(table, ref result_size);
+            double[] result = new double[result_size];
+            table_result = S.Calculate(result);
+
+            Console.WriteLine("Решенная симплекс-таблица:");
+            for (int i = 0; i < table_result.GetLength(0); i++)
+            {
+                for (int j = 0; j < table_result.GetLength(1); j++)
+                    Console.Write(table_result[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Решение:");
+            for(var x = 1; x <= result.Length; x++)
+                Console.WriteLine("X" + x.ToString() + "=" + result[x-1]);
+            //Console.WriteLine("X[2] = " + result[1]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ simplex_method/Simplex.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simplex_method
+{
+    public class Simplex
+    {
+        //source - симплекс таблица без базисных переменных
+        double[,] table; //симплекс таблица
+
+        int m, n;
+
+        List<int> basis; //список базисных переменных
+
+        public Simplex(double[,] source, ref int result_size)
+        {
+            m =source.GetLength(0); // берем высоту симплекс таблицы 
+            n = source.GetLength(1); // берем длину симплекс таблицы
+            table = new double[m, n + m - 1]; // определяем размеры массива (количество уранений + строка для целевой на ббазисные + свободные))
+            basis = new List<int>();
+            
+            for (int i = 0; i < m; i++) // цикл по строкам СТ
+            {
+                for (int j = 0; j < table.GetLength(1); j++) //цикл по столбцам СТ
+                {
+                    //if(i == (m - 1))
+
+                    if (j < n) // если свободная переменная
+                        table[i, j] = source[i, j]; 
+                    else
+                        table[i, j] = 0;
+                }
+                //выставляем коэффициент 1 перед базисной переменной в строке
+                if ((n + i) < table.GetLength(1))
+                {
+                    table[i, n + i] = 1; // присваиваем значение соотв. базисной переменной
+                    basis.Add(n + i);
+                }
+            }
+
+            n = table.GetLength(1); // переназначаем длину СТ
+            result_size = n - 1; // определяем число переменных в решении
+        }
+
+        //result - в этот массив будут записаны полученные значения X
+        public double[,] Calculate(double[] result)
+        {
+            int mainCol, mainRow; //ведущие столбец и строка
+
+            while (!IsItEnd())
+            {
+                mainCol = findMainCol();
+                mainRow = findMainRow(mainCol);
+                basis[mainRow] = mainCol; // поменяли местами ведущий стобец и строку
+
+                double[,] new_table = new double[m, n]; // задаем размеры матрицы новой
+
+                for (int j = 0; j < n; j++)
+                    new_table[mainRow, j] = table[mainRow, j] / table[mainRow, mainCol]; // элементы главной строки делим на ведущий элемент
+
+                for (int i = 0; i < m; i++) // перебираем все строки в таблице 
+                {
+                    if (i == mainRow) // кроме самой строки
+                        continue;
+
+                    for (int j = 0; j < n; j++)
+                        new_table[i, j] = table[i, j] - table[i, mainCol] * new_table[mainRow, j];  //  каждый элемент новой СТ ведущей строки умножается на ведущий элемент с минусом и результат произведения вычетаем из соответствующего значения нвоой СТ
+                }
+                table = new_table;
+            }
+
+            //заносим в result найденные значения X
+            for (int i = 0; i < result.Length; i++)
+            {
+                int k = basis.IndexOf(i + 1);
+                if (k != -1) // записываем в опорное решение значения БП 
+                    result[i] = table[k, 0];
+                else
+                    result[i] = 0;
+            }
+
+            return table;
+        }
+
+        private bool IsItEnd()
+        {
+            bool flag = true;
+
+            for (int j = 1; j < n; j++)
+            {
+                if (table[m - 1, j] < 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            return flag;
+        }
+
+        private int findMainCol() // ищем наибольшее по модулю отрицательное значение в коэф. целевой функции в СТ 
+        {
+            int mainCol = 1;
+
+            for (int j = 2; j < n; j++)
+                if (table[m - 1, j] < table[m - 1, mainCol])
+                    mainCol = j;
+
+            return mainCol;
+        }
+
+        private int findMainRow(int mainCol)
+        {
+            int mainRow = 0;
+
+            for (int i = 0; i < m - 1; i++)
+                if (table[i, mainCol] > 0)
+                {
+                    mainRow = i;
+                    break;
+                }
+
+            for (int i = mainRow + 1; i < m - 1; i++)
+		{
+ 		// проверка на наименьшее отношение элементов
+                if ((table[i, mainCol] > 0) && ((table[i, 0] / table[i, mainCol]) < (table[mainRow, 0] / table[mainRow, mainCol])))
+                    mainRow = i;
+		}
+            return mainRow;
+        }
+
+
+    }
+}
